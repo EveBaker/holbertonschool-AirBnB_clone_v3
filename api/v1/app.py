40b35e806f47741cac27f208cf8e48d69b817e31
EveBaker <EveBaker0162@gmail.com>
2023-07-22 10:53:47 -0700

    fixed imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-app.py to connect to API
-"""
+"""app.py to connect to API"""
 import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
-
 
 
 app = Flask(__name__)
