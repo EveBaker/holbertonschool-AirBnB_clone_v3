b9a8976ebf9bfe9bb1dcd98ea5deabaf7077dc60
gumquat <gumquat@gmail.com>
2023-07-22 13:16:43 -0500

    session thing fixed maybe?

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,7 +71,7 @@ class FileStorage:
 
     def get(self, cls, id):
         """Retrieves an obj, based on the class and its ID."""
-        obj = self.__session.query(cls).filter_by(id=id).first()
+        obj = self.query(cls).filter_by(id=id).first()
         return obj
     
     def count(self, cls=None):
