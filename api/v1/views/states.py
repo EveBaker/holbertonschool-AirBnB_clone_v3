8fd39d76f29c4d0b5617ca65387999971dee3395
gumquat <gumquat@gmail.com>
2023-07-23 14:05:31 -0500

    added imports

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,6 +3,8 @@
 all default RESTFul API actions"""
 from flask import Blueprint, jsonify, request, abort
 from models import State
+from models import storage
+from api.v1.views import app_views
 
 
 bp = Blueprint('states', __name__)
