3ea1d6d22a4626a0a6709cc8baed3cebd32a5cbb
gumquat <gumquat@gmail.com>
2023-07-24 12:09:06 -0500

    revert to __init__.py from the beggining

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""init BP views"""
-import json
-from api.v1.views import states_bp
+"""Initialize Blueprint views"""
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
 from api.v1.views.states import *
-
-
-def load_states():
-    with open('states.json', 'r') as f:
-        data = json.load(f)
-
-    for state in data:
-        state.create(name=state['name'])
-
-
-def init_app(app):
-    app.register_blueprint(states_bp)
-    
-    load_states()
+from api.v1.views.cities import *
+from api.v1.views.users import *
