9a9ec72f896ee46132fc83a0b1e3fa35833e3851
EveBaker <EveBaker0162@gmail.com>
2023-07-24 14:39:54 -0700

    work in prog

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -23,3 +23,40 @@ def get_user(user_id):
     if not user:
         abort(404)
     return jsonify(user.to_dict())
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """ Deletes a User object """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+    """ Creates a User """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    if 'email' not in request.get_json():
+        abort(400, description="Missing email")
+    if 'password' not in request.get_json():
+        abort(400, description="Missing password")
+    user = User(**request.get_json())
+    user.save()
+    return make_response(jsonify(user.to_dict()), 201)
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def put_user(user_id):
+    """ Updates a User object """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    for attr, val in request.get_json().items():
+        if attr not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, attr, val)
+    user.save()
+    return jsonify(user.to_dict())
