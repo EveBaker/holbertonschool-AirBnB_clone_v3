80f4e989fc1f35efb4f44deae24fe41e0061579d
EveBaker <EveBaker0162@gmail.com>
2023-07-24 14:17:16 -0700

    work in progress

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,7 +5,21 @@ from flask import abort, jsonify, make_response, request
 from models import storage
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """ Retrieves the list of all User objects """
     all_users = storage.all(User).values()
+    list_users = []
+    for user in all_users:
+        list_users.append(user.to_dict())
+        return jsonify(list_users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """ Retrieves a User object """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
