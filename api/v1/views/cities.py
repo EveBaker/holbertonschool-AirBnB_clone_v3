450652562589ff104234aaf9f7f9b14538a4a9b3
EveBaker <EveBaker0162@gmail.com>
2023-07-24 13:59:41 -0700

    fixed p8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,6 +6,7 @@ from models import storage
 from models.state import State
 from models.city import City
 
+
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
 def get_cities(state_id):
     state = storage.get(State, state_id)
@@ -13,12 +14,14 @@ def get_cities(state_id):
         abort(404)
     return jsonify([city.to_dict() for city in state.cities])
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'])
 def get_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
@@ -28,6 +31,7 @@ def delete_city(city_id):
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def create_city(state_id):
@@ -41,6 +45,7 @@ def create_city(state_id):
     city = City(name=request.get_json()['name'], state_id=state_id)
     city.save()
     return jsonify(city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def update_city(city_id):
