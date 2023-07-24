6e1bdceb1c1e2d61325faaceb9bdac2808156594
EveBaker <EveBaker0162@gmail.com>
2023-07-23 18:04:18 -0700

    work in progress

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -33,29 +33,4 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'])
-def create_city(state_id):
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
-    if 'name' not in request.get_json():
-        abort(400, 'Missing name')
-    city = City(name=request.get_json()['name'], state_id=state_id)
-    city.save()
-    return jsonify(city.to_dict()), 201
 
-
-@app_views.route('/cities/<city_id>', methods=['PUT'])
-def update_city(city_id):
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
-    for key, value in request.get_json().items():
-        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
-            setattr(city, key, value)
-    city.save()
-    return jsonify(city.to_dict()), 200
