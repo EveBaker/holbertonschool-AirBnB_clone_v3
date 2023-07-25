93a367ea19f2daf226f5b37530a7aa5d5db0943d
gumquat <gumquat@gmail.com>
2023-07-25 12:44:21 -0500

    edit to amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""states.py"""
-
+"""Create a new view for Amenity objects
+that handles all default RESTFul API actions:"""
+from models.amenity import Amenity
+from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from models import storage
-from models.amenity import Amenity
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities',
+                 methods=['GET'], strict_slashes=False)
 def get_amenities():
     """get amenity information for all amenities"""
     amenities = []
@@ -15,9 +16,8 @@ def get_amenities():
         amenities.append(amenity.to_dict())
     return jsonify(amenities)
 
-
-@app_views.route('/amenities/<string:amenity_id>', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/amenities/<string:amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """get amenity information for specified amenity"""
     amenity = storage.get("Amenity", amenity_id)
@@ -25,9 +25,8 @@ def get_amenity(amenity_id):
         abort(404)
     return jsonify(amenity.to_dict())
 
-
-@app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/amenities/<string:amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """deletes an amenity based on its amenity_id"""
     amenity = storage.get("Amenity", amenity_id)
@@ -37,8 +36,8 @@ def delete_amenity(amenity_id):
     storage.save()
     return (jsonify({}))
 
-
-@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+@app_views.route('/amenities',
+                 methods=['POST'], strict_slashes=False)
 def post_amenity():
     """create a new amenity"""
     if not request.get_json():
@@ -49,9 +48,8 @@ def post_amenity():
     amenity.save()
     return make_response(jsonify(amenity.to_dict()), 201)
 
-
-@app_views.route('/amenities/<string:amenity_id>', methods=['PUT'],
-                 strict_slashes=False)
+@app_views.route('/amenities/<string:amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def put_amenity(amenity_id):
     """update an amenity"""
     amenity = storage.get("Amenity", amenity_id)
