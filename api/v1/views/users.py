f42cf00c730c7a58504d6ca01abb19aec6611b87
EveBaker <EveBaker0162@gmail.com>
2023-07-25 08:51:10 -0700

    p8

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -48,6 +48,7 @@ def post_user():
     user.save()
     return make_response(jsonify(user.to_dict()), 201)
 
+
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
     """ Updates a User object """
