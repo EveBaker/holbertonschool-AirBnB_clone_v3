69ff4d383d45bdbd049595ae8c845e1d171a8400
EveBaker <EveBaker0162@gmail.com>
2023-07-25 10:27:30 -0700

    work in prog

---
+++ api/v1/views/places.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""places.py"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.city import City
+from models.place import Place
+from models.user import User
