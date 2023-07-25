4d018c39cf120b6865c7987f5f8212e5a2cba130
EveBaker <EveBaker0162@gmail.com>
2023-07-25 08:47:54 -0700

    user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -9,11 +9,10 @@ from models.user import User
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """ Retrieves the list of all User objects """
-    all_users = storage.all(User).values()
-    list_users = []
-    for user in all_users:
-        list_users.append(user.to_dict())
-        return jsonify(list_users)
+    users = []
+    for user in storage.all("User").values():
+        users.append(user.to_dict())
+        return jsonify(users)
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
@@ -31,24 +30,23 @@ def delete_user(user_id):
     user = storage.get(User, user_id)
     if not user:
         abort(404)
-    storage.delete(user)
+    user.delete(user)
     storage.save()
-    return make_response(jsonify({}), 200)
+    return (jsonify({}))
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def post_user():
     """ Creates a User """
     if not request.get_json():
-        abort(400, description="Not a JSON")
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'email' not in request.get_json():
-        abort(400, description="Missing email")
+        return make_response(jsonify({'error': 'Missing email'}), 400)
     if 'password' not in request.get_json():
-        abort(400, description="Missing password")
+        return make_response(jsonify({'error': 'Missing password'}), 400)
     user = User(**request.get_json())
     user.save()
     return make_response(jsonify(user.to_dict()), 201)
-
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
